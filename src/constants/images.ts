f6dad352f6b3cef52d2c1d8da94e0e5bef5d8f14
Diogo Ferreira <diogocraft.2@gmail.com>
2023-02-18 17:04:54 -0300

    feat(constants): adicionadas constantes de estilização da aplicação

---
+++ src/constants/images.ts
@@ -0,0 +1,16 @@
+import wallieLogo from "../assets/images/wallie-logo.png";
+import banner from "../assets/images/banner.png";
+import promoBanner from "../assets/images/promo-banner.png";
+import focus from "../assets/images/focus.png";
+
+import usFlag from "../assets/images/us-flag.jpg";
+
+export default {
+  wallieLogo,
+  banner,
+  promoBanner,
+  focus,
+
+  // Dummy
+  usFlag
+}
